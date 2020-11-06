d0e2f127e48044eec36245fe7dc96991509453de
Jonas Borjesson <jonas@jonasborjesson.com>
2020-11-05 23:36:29 -0800

    more stuff

--- networking-examples/src/main/java/io/snice/networking/examples/gtp/PgwGtpC.java
+++ networking-examples/src/main/java/io/snice/networking/examples/gtp/PgwGtpC.java
@@ -1,14 +1,13 @@
 package io.snice.networking.examples.gtp;
 
 import io.snice.buffer.Buffers;
+import io.snice.codecs.codec.MccMnc;
 import io.snice.codecs.codec.gtp.Teid;
 import io.snice.codecs.codec.gtp.gtpc.v2.Gtp2Message;
 import io.snice.codecs.codec.gtp.gtpc.v2.Gtp2MessageType;
 import io.snice.codecs.codec.gtp.gtpc.v2.messages.path.EchoRequest;
 import io.snice.codecs.codec.gtp.gtpc.v2.tliv.*;
-import io.snice.codecs.codec.gtp.gtpc.v2.type.FTeidType;
-import io.snice.codecs.codec.gtp.gtpc.v2.type.PdnType;
-import io.snice.codecs.codec.gtp.gtpc.v2.type.TbcdType;
+import io.snice.codecs.codec.gtp.gtpc.v2.type.*;
 import io.snice.codecs.codec.tgpp.ReferencePoint;
 import io.snice.networking.app.NetworkBootstrap;
 import io.snice.networking.common.Connection;
@@ -52,26 +51,59 @@ public class PgwGtpC extends GtpApplication<GtpConfig> {
 
     @Override
     public void run(final GtpConfig configuration, final GtpEnvironment<GtpConfig> environment) {
-        environment.connect(Transport.udp, "3.88.129.158", 2123).thenAccept(c -> {
-            final var ftied = FTeidType.create()
-                    .withIPv4Address("192.168.0.100")
-                    .withReferencePoint(ReferencePoint.S5, true)
-                    .withTeid(Teid.ZEROS)
-                    .build();
+        final var arp = ArpType.ofValue(7, true, true);
+        final var qos = QosType.ofQci(2).build();
+        final var bqos = BearerQos.ofValue(BearerQosType.ofValue(arp, qos));
 
-            final var f = FTeid.ofValue(ftied);
+        final var ftiedType = FTeidType.create()
+                .withIPv4Address("192.168.0.100")
+                .withReferencePoint(ReferencePoint.S5, true)
+                .withTeid(Teid.ZEROS)
+                .build();
+
+        final var ftied = FTeid.ofValue(FTeidType.create()
+                .withIPv4Address("192.168.0.100")
+                .withReferencePoint(ReferencePoint.S5, true)
+                .withTeid(Teid.ZEROS)
+                .build());
+
+        final var ebi = Ebi.ofValue(EbiType.ofValue(5));
+        final var grouped = GroupedType.ofValue(bqos, ftied, ebi);
+
+        final var bearerContext = BearerContext.ofValue(grouped);
+
+        final var tac = Buffers.wrap((byte) 0x11, (byte) 0x22);
+        final var tai = TaiField.of(MccMnc.of("123", "12"), tac);
+        final var uli = UliType.create().withTai(tai).build();
+
+        final var servingNetwork = ServingNetwork.ofValue(MccMncType.ofValue("310/410"));
+        final var ambr = Ambr.ofValue(AmbrType.ofValue(10000, 20000));
+
+        System.err.println("===== starting to run ====");
+        environment.connect(Transport.udp, "3.89.25.140", 2123).thenAccept(c -> {
+
+            System.err.println("Whats going on?");
+
+            final var f = FTeid.ofValue(ftiedType);
 
             final var csr = Gtp2Message.create(Gtp2MessageType.CREATE_SESSION_REQUEST)
                     .withTeid(Teid.of(Buffers.wrap((byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00)))
                     .withTliv(f)
-                    .withTliv(Apn.ofValue("super.hello"))
+                    // TODO: fix grouped. And fix all IE of type grouped. Like I did for diameter
+                    .withTliv(Uli.ofValue(uli))
+                    .withTliv(bearerContext)
+                    .withTliv(servingNetwork)
+                    .withTliv(ambr)
+                    .withTliv(ApnRestriction.ofValue(CounterType.parse("0")))
+                    .withTliv(Paa.ofValue(PaaType.fromIPv4("10.11.12.13")))
+                    .withTliv(SelectionMode.ofValue(SelectionModeType.ofValue(0)))
+                    .withTliv(Apn.ofValue("super"))
                     .withTliv(Pdn.ofValue(PdnType.of(PdnType.Type.IPv4)))
                     .withTliv(Mei.ofValue(TbcdType.parse("123456789")))
                     .withTliv(Rat.ofValue("6"))
-                    .withTliv(Imsi.ofValue("001001123456"))
+                    .withTliv(Imsi.ofValue("999994000000642"))
                     .withTliv(Msisdn.ofValue("41555512345"))
                     .build();
-
 
             final var evt = GtpMessageWriteEvent.of(csr, c);
             c.send(evt);
